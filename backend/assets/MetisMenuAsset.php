0db3575f05fc2174b9679783efc87955e7cd3769
Cosmo <daixianceng@gmail.com>
2016-01-31 13:43:59 +0800

    添加Comment

--- backend/assets/MetisMenuAsset.php
+++ backend/assets/MetisMenuAsset.php
@@ -4,7 +4,9 @@ namespace backend\assets;
 
 use yii\web\AssetBundle;
 
-
+/**
+ * @author Cosmo <daixianceng@gmail.com>
+ */
 class MetisMenuAsset extends AssetBundle
 {
     // The files are not web directory accessible, therefore we need
